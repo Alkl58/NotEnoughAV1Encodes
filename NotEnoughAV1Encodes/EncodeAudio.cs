cc73fb6aa7c62aceb0c3163aa3e2718fdc1b9730
Alkl58 <alkl@web.de>
2020-04-25 20:10:57 +0200

    Added Audio Copy - Started using switch cases

--- NotEnoughAV1Encodes/EncodeAudio.cs
+++ NotEnoughAV1Encodes/EncodeAudio.cs
@@ -13,36 +13,107 @@ namespace NotEnoughAV1Encodes
                 string audioInput = "";
                 string audioMapping = "";
                 string audioCodec = "";
+                bool skipbitrate = false;
                 //Creates AudioEncoded Directory in the temp dir
                 if (!Directory.Exists(Path.Combine(MainWindow.workingTempDirectory, "AudioEncoded")))
                     Directory.CreateDirectory(Path.Combine(MainWindow.workingTempDirectory, "AudioEncoded"));
                 //Audio Encoder --------------------------------------------------------------------------||
-                if (MainWindow.audioCodec == "Opus") { audioCodec = "libopus"; }
+                //Audio Codec Switch
+                switch (MainWindow.audioCodec)
+                {
+                    case "Opus":
+                        audioCodec = "libopus";
+                        break;
+                    case "Opus 5.1":
+                        audioCodec = "libopus -af channelmap=channel_layout=5.1";
+                        break;
+                    case "Opus Downmix":
+                        audioCodec = "libopus -ac 2";
+                        break;
+                    case "AC3":
+                        audioCodec = "ac3";
+                        break;
+                    case "AAC":
+                        audioCodec = "aac";
+                        break;
+                    case "MP3":
+                        audioCodec = "libmp3lame";
+                        break;
+                    case "Copy Audio":
+                        audioCodec = "copy";
+                        skipbitrate = true;
+                        break;
+                    default:
+                        break;
+                }
+                /*
+                if (MainWindow.audioCodec == "Opus") {  }
                 if (MainWindow.audioCodec == "Opus 5.1") { audioCodec = "libopus -af channelmap=channel_layout=5.1"; }
                 if (MainWindow.audioCodec == "Opus Downmix") { audioCodec = "libopus -ac 2"; }
                 if (MainWindow.audioCodec == "AC3") { audioCodec = "ac3"; }
                 if (MainWindow.audioCodec == "AAC") { audioCodec = "aac"; }
-                if (MainWindow.audioCodec == "MP3") { audioCodec = "libmp3lame"; }
+                if (MainWindow.audioCodec == "MP3") { audioCodec = "libmp3lame"; } */
                 //----------------------------------------------------------------------------------------||
                 //Audio Mapping --------------------------------------------------------------------------||
                 if (MainWindow.trackOne == true && MainWindow.detectedTrackOne == true)
                 {
-                    audioInput += " -c:a:0 " + audioCodec + " -b:a:0 " + MainWindow.audioBitrate + "k";
+                    switch (skipbitrate)
+                    {
+                        case true:
+                            audioInput += " -c:a:0 " + audioCodec;
+                            break;
+                        case false:
+                            audioInput += " -c:a:0 " + audioCodec + " -b:a:0 " + MainWindow.audioBitrate + "k";
+                            break;
+                        default:
+                            break;
+                    }
+                    
                     audioMapping += " -map 0:"+ MainWindow.firstTrackIndex + " ";
                 }
                 if (MainWindow.trackTwo == true && MainWindow.detectedTrackTwo == true)
                 {
-                    audioInput += " -c:a:1 " + audioCodec + " -b:a:1 " + MainWindow.audioBitrate + "k";
+                    switch (skipbitrate)
+                    {
+                        case true:
+                            audioInput += " -c:a:1 " + audioCodec;
+                            break;
+                        case false:
+                            audioInput += " -c:a:1 " + audioCodec + " -b:a:1 " + MainWindow.audioBitrate + "k";
+                            break;
+                        default:
+                            break;
+                    }
                     audioMapping += " -map 0:" + MainWindow.secondTrackIndex + " ";
                 }
                 if (MainWindow.trackThree == true && MainWindow.detectedTrackThree == true)
                 {
-                    audioInput += " -c:a:2 " + audioCodec + " -b:a:2 " + MainWindow.audioBitrate + "k";
+                    switch (skipbitrate)
+                    {
+                        case true:
+                            audioInput += " -c:a:2 " + audioCodec;
+                            break;
+                        case false:
+                            audioInput += " -c:a:2 " + audioCodec + " -b:a:2 " + MainWindow.audioBitrate + "k";
+                            break;
+                        default:
+                            break;
+                    }
                     audioMapping += " -map 0:" + MainWindow.thirdTrackIndex + " ";
                 }
                 if (MainWindow.trackFour == true && MainWindow.detectedTrackFour == true)
                 {
-                    audioInput += " -c:a:3 " + audioCodec + " -b:a:3 " + MainWindow.audioBitrate + "k";
+                    switch (skipbitrate)
+                    {
+                        case true:
+                            audioInput += " -c:a:3 " + audioCodec;
+                            break;
+                        case false:
+                            audioInput += " -c:a:3 " + audioCodec + " -b:a:3 " + MainWindow.audioBitrate + "k";
+                            break;
+                        default:
+                            break;
+                    }
                     audioMapping += " -map 0:" + MainWindow.fourthTrackIndex + " ";
                 }
                 //----------------------------------------------------------------------------------------||
