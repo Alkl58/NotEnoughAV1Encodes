a15d0014cf3e42404c48c30f266d4c0afc4514e0
Alkl58 <alexandrekaul@protonmail.com>
2020-11-13 12:13:54 +0100

    Remove unnecessary code from RenameChunks

--- NotEnoughAV1Encodes/RenameSplits.cs
+++ NotEnoughAV1Encodes/RenameSplits.cs
@@ -7,33 +7,29 @@ namespace NotEnoughAV1Encodes
     {
         public static void Rename()
         {
-            string[] chunks;
+            // Sets the Path of the Directory with the Video Chunks
             string chunkDir = Path.Combine(MainWindow.TempPath, MainWindow.TempPathFileName, "Chunks");
-            chunks = Directory.GetFiles(chunkDir, "*mkv", SearchOption.AllDirectories).Select(x => Path.GetFileName(x)).ToArray();
-            DirectoryInfo d = new DirectoryInfo(chunkDir);
-            FileInfo[] infos = d.GetFiles();
-            int numberOfChunks = chunks.Count();
 
-            foreach (FileInfo f in infos)
+            // Creates a new directory info for the dir with the chunks
+            DirectoryInfo fileDir = new DirectoryInfo(chunkDir);
+
+            // Creates a FileInfo Array with the files from the DirectoryInfo
+            FileInfo[] infos = fileDir.GetFiles();
+
+            // Iterates over all files in Chunk Directory
+            foreach (FileInfo file in infos)
             {
+                // Gets the length of the filename
                 int count = f.ToString().Count();
+
+                // Depending on the file length it will rename them
                 switch (count)
                 {
-                    case 9:
-                        File.Move(d + "\\" + f, d + "\\" + f.Name.Replace("split", "split00000"));
-                        break;
-                    case 10:
-                        File.Move(d + "\\" + f, d + "\\" + f.Name.Replace("split", "split0000"));
-                        break;
-                    case 11:
-                        File.Move(d + "\\" + f, d + "\\" + f.Name.Replace("split", "split000"));
-                        break;
-                    case 12:
-                        File.Move(d + "\\" + f, d + "\\" + f.Name.Replace("split", "split00"));
-                        break;
-                    case 13:
-                        File.Move(d + "\\" + f, d + "\\" + f.Name.Replace("split", "split0"));
-                        break;
+                    case 9: File.Move(fileDir + "\\" + file, fileDir + "\\" + file.Name.Replace("split", "split00000")); break;
+                    case 10: File.Move(fileDir + "\\" + file, fileDir + "\\" + file.Name.Replace("split", "split0000")); break;
+                    case 11: File.Move(fileDir + "\\" + file, fileDir + "\\" + file.Name.Replace("split", "split000")); break;
+                    case 12: File.Move(fileDir + "\\" + file, fileDir + "\\" + file.Name.Replace("split", "split00")); break;
+                    case 13: File.Move(fileDir + "\\" + file, fileDir + "\\" + file.Name.Replace("split", "split0")); break;
                 }
             }
         }
