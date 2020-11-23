d4844bd9686262fdccdc606191f384c03c9c4c32
Alkl58 <alexandrekaul@protonmail.com>
2020-11-23 12:40:08 +0100

    Move Save Setting Function

--- NotEnoughAV1Encodes/SavePreset.xaml.cs
+++ NotEnoughAV1Encodes/SavePreset.xaml.cs
@@ -4,12 +4,15 @@ namespace NotEnoughAV1Encodes
 {
     public partial class SavePreset : Window
     {
+        public string SaveName { get; set; }
+        public bool cancel { get; set; }
         public SavePreset()
         {
             InitializeComponent();
         }
         private void ButtonCloseWindow_Click(object sender, RoutedEventArgs e)
         {
+            cancel = true;
             // Closes the Window
             this.Close();
         }
@@ -17,7 +20,9 @@ namespace NotEnoughAV1Encodes
         private void ButtonSavePreset_Click(object sender, RoutedEventArgs e)
         {
             // Saves the Preset
-            SaveSettings saveSettings = new SaveSettings(true, TextBoxPresetName.Text);
+            SaveName = TextBoxPresetName.Text;
+            cancel = false;
+
             // Closes the Window
             this.Close();
         }
