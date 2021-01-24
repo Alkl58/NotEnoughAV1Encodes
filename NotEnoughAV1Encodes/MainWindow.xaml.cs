b52fcd7a52c854375e879db54d10663ed5daddbc
Alkl58 <alexandrekaul@protonmail.com>
2021-01-24 18:41:05 +0100

    Fix Audio Track Detection (again)

--- NotEnoughAV1Encodes/MainWindow.xaml.cs
+++ NotEnoughAV1Encodes/MainWindow.xaml.cs
@@ -1469,7 +1469,7 @@ namespace NotEnoughAV1Encodes
                 WindowStyle = ProcessWindowStyle.Hidden,
                 FileName = "cmd.exe",
                 WorkingDirectory = FFmpegPath,
-                Arguments = "/C ffprobe.exe -i " + '\u0022' + VideoInput + '\u0022' + " -loglevel error -select_streams a -show_entries stream=codec_type,index -of csv=p=0",
+                Arguments = "/C ffprobe.exe -i " + '\u0022' + VideoInput + '\u0022' + " -loglevel error -select_streams a -show_entries stream=index -of csv=p=1",
                 RedirectStandardError = true,
                 RedirectStandardOutput = true
             };
@@ -1478,7 +1478,7 @@ namespace NotEnoughAV1Encodes
             string audioIndexes = getAudioIndexes.StandardOutput.ReadToEnd();
             getAudioIndexes.WaitForExit();
             //Splits the Console Output
-            string[] audioIndexesFixed = audioIndexes.Split(new string[] { " ", ",audio" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] audioIndexesFixed = audioIndexes.Split(new string[] { " ", "stream," }, StringSplitOptions.RemoveEmptyEntries);
             int detectedTracks = 0;
             bool trackone = false, tracktwo = false, trackthree = false, trackfour = false;
             // Iterates over the audioIndexesFixed Array
