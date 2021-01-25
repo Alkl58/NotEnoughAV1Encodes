f6cbff83abcc00e76286a23e44085c7b4d507f90
Alkl58 <alexandrekaul@protonmail.com>
2021-01-25 14:59:39 +0100

    Improved Popup Window

--- NotEnoughAV1Encodes/PopupWindow.xaml.cs
+++ NotEnoughAV1Encodes/PopupWindow.xaml.cs
@@ -6,6 +6,7 @@ namespace NotEnoughAV1Encodes
 {
     public partial class PopupWindow : MetroWindow
     {
+        public static string fileLocation;
         public PopupWindow(string baseTheme, string accentTheme, string time, string frames, string avg, string fileName)
         {
             InitializeComponent();
@@ -14,12 +15,20 @@ namespace NotEnoughAV1Encodes
             LabelEncodedFrames.Content = frames;
             LabelEncodedAVG.Content = avg;
             FileInfo fs = new FileInfo(fileName);
+            fileLocation = fileName;
             LabelEncodedFilesize.Content = (fs.Length >> 20) + "MB";
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            // Closes this window
             this.Close();
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Opens the video output file location and select the video file
+            System.Diagnostics.Process.Start("explorer.exe", "/select, \"" + fileLocation + "\"");
         }
     }
 }
